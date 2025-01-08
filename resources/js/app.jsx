07b80e42b54b99371f20fa34d730eda2647179bb
Knathip310 <dungknathip@gmail.com>
2025-01-08 17:10:28 +0700

    Initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,27 @@
+
+//โค้ดนี้คือการกำหนดค่าแอปพลิเคชันในเชิงโครงสร้าง (modular configuration) เพื่อแยกการตั้งค่า routing, middleware, และข้อยกเว้นให้อยู่ในที่เดียวกัน
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
